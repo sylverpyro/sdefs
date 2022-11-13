62405b65dff5c8511f65f322f97fe4dd46e8d867
sylverpyro <sylverpyro@users.noreply.github.com>
2022-11-13 12:53:03 -0600

    Lots of updates and README disclaimers/notes
    - Swapped Nestopia-UE for Retroarch:FCEUmm
     - Nestopia-UE has a 'unable to quit' bug when it's in fullscreen
     - Retroarch:FCEUmm has better performance that Retroarch:Nestopia (in my subjective testing)
    - Added Duckstation for dedicated PSX
    - First pass at BIOS detection for DuckStation (so far)

--- README.md
+++ README.md
@@ -13,10 +13,45 @@ This should in theory be compatible with any other FlatPak Linux disto, but deve
 
 # What it should do
 - Install FlatPak versions of each emulator (exact list pending) from the SD flatpak CLI
-- Link the imprtant data folders for each emulator to a common location (e.g. saves, screenshots, ect.)
+  - Only use RetroArch cores if the FlatPak emulators don't play well with Gampads
+- Link the imprtant data folders for each emulator/core to a common location (e.g. saves, screenshots, ect.)
 - Install a default config file for the controls and semi-good visual settings
-- Set any permissions on the 
+- Allow the user to change the configurations as they see fit, however they see fit
+- Provide a 'config reset' option in case something get's changed that really messes up their experience
 
-# What is should NOT do
-- Prevent users from modifying their configurations of each emulator
-- Delete anything
+# Assumptions/Design Contraints
+- Assume a standard 'EmulationStation-DE' rom folder structure
+ - This is required to be able to 'guess' content directory names for RetroArch
+
+# Known issues
+- Right now assumes the rom folder is on an SD card (standard ES-DE location)
+
+# Incomplete Things
+- Does not install/modify config files to setup mappings yet (coming later)
+- Does not check for/link BIOS files to their proper locations (coming later)
+- Does not modify EmulationStation-DE default emulator commands to match installed emulators (research needed)
+
+# Annoying Things: RetroArch
+- SteamDeck's "gamepad" mode doesn't have dedicated input events for L/R 4 and 5 (at least not ones that RetroArch understands)
+ - Workaround Mappings:
+ - L4 = ;
+ - L5 = '
+ - R4 = ,
+ - R5 = .
+- Some games use 'Select' for the AmberELEC convetion of 'select' being a Hotkey-Enable causes gameplay problems
+ - Workaround: L4 (;) is mapped to Hotkey enable where possible
+
+# Annoying Things: DuckStation
+- Doesn't have a 'hotkey enable' mode so all keys immediately do hotkey functions
+- Workaround: Use the community controler bindings to enable a hotkey radial menu on the left mousepad
+
+# Hotkey Setup: Retroarch
+- Exit       : L4 + Start
+- FF         : L4 + R2
+- Save State : L4 + R1
+- Load State : L4 + L1
+- Screenshot : L4 + L2
+- Menu       : L3 + R3 (Retroarch)
+
+# Additoinal Setup: DuckStation
+- When adding DuckStation to steam - edit the command to include '-bigpicture' to launch in Steamdeck compatable mode
--- sdefs.sh
+++ sdefs.sh
@@ -2,14 +2,22 @@
 set -o nounset
 
 function defaults() {
-  export emu_data="/run/media/mmcblk0p1/Emulation/data"
+  export emudata="/run/media/mmcblk0p1/Emulation/data"
   export script_log="/tmp/$_script.log"
 }
 
-function mk_dirs() {
-  echo "Creating emudata folder: '$emu_data'"
-  mkdir -p "$emu_data"
+function safe_mkdir() {
+  if [ ! -d "$1" ]; then
+    echo "Creating Directory: '$1'"
+    mkdir -p "$1"
+  fi
+}
 
+function safe_mklnk() {
+  if [ ! -L "$2" ]; then
+    echo "Linking '$2' -> '$1'"
+    ln -s "$1" "$2"
+  fi
 }
 
 function setup_flatpak() {
@@ -33,14 +41,75 @@ function setup_flatpak() {
   fi
 }
 
+function check_bios() {
+  case "$1" in
+    'org.duckstation.DuckStation')
+      local _biosdir="$HOME/.var/app/org.duckstation.DuckStation/config/duckstation/bios"
+      for _file in scph5500.bin scph5501.bin scph5502.bin scph5503.bin scph7003.bin; do
+        if [ -f "$_biosdir/$_file" ]; then
+          echo "DuckStation BIOS File: $_file : Present"
+        else
+          echo "DuckStation BIOS File: $_file : Missing - Install with 'cp FILE $_biosdir/'"
+        fi
+      done
+      ;;
+  esac
+}
+
 function setup_emudata() {
   case "$1" in
+    'org.duckstation.DuckStation')
+      local _emudata="$emudata/duckstation"
+      local _vardata="$HOME/.var/app/org.duckstation.DuckStation/config/duckstation/memcards"
+      local _bios="$HOME/.var/app/org.duckstation.DuckStation/config/duckstation/bios"
+      safe_mkdir "$_emudata"
+      # DuckStation has 3 data directories to link
+      safe_mklnk "$_vardata/memcards" "$_emudata/saves"
+      safe_mklnk "$_vardata/screenshots" "$_emudata/screenshots"
+      safe_mklnk "$_vardata/savestates" "$_emudata/savestates"      
+      safe_mklnk "$_bios" "$_emudata/bios"
+      echo "DuckStation: Emudata linked to: $_emudata"
+      check_bios "$1"
+    ;;
+
     'ca._0ldsk00l.Nestopia')
+      local _emudata="$emudata/nestopia-ue"
+      local _vardata="$HOME/.var/app/ca._0ldsk00l.Nestopia/data/nestopia"
+      safe_mkdir "$_emudata"
       # Nestopia has 3 directories of interest we want to link-out
       for _dir in save screenshots state; do
-        if [ ! -L "$emu_data/nestopia-ue/$_dir" ]; then
-          ln -s "~/.var/app/ca._0ldsk00l.Nestopia/data/nestopia/$_dir" "$emu_data/nestopia-ue"
-        fi
+        safe_mklnk "$_vardata/$_dir" "$_emudata/$_dir"
+      done
+      ;;
+
+    'org.libretro.RetroArch')
+      # There's several cores here - so set them all up
+      # NOTE: This doesn't have to exist yet, we can still set up the symlinks now
+      local _vardata="$HOME/.var/app/org.libretro.RetroArch/config/retroarch"
+      # List of RetroArch cores to set up
+      # In personal, opinionated testing FCEUmm gave better performance than Nestopia even
+      # if Nestopia is more accurate
+      for _enabled in FCEUmm; do
+        case $_enabled in
+          'Nestopia')
+            # Nestopia
+            local _emudata="$emudata/retroarch-nestopia"
+            safe_mkdir "$_emudata"
+            # Retroarch sorts saves by core name, but sreenshots by content-dir
+            safe_mklnk "$_vardata/saves/Nestopia" "$_emudata/saves"
+            safe_mklnk "$_vardata/screenshots/nes" "$_emudata/screenshots"
+            #echo "Retroarch-Nestopia: Emudata linked to $_emudata"
+          ;;
+          'FCEUmm')
+            local _emudata="$emudata/retroarch-fceumm"
+            safe_mkdir "$_emudata"
+            # Retroarch sorts saves by core name, but sreenshots by content-dir
+            safe_mklnk "$_vardata/saves/FCEUmm" "$_emudata/saves"
+            safe_mklnk "$_vardata/states/FCEUmm" "$_emudata/saves"
+            safe_mklnk "$_vardata/screenshots/nes" "$_emudata/screenshots"
+            echo "Retroarch-FCEUmm: Emudata linked to $_emudata"
+          ;;
+        esac
       done
       ;;
 
@@ -54,8 +123,17 @@ function setup_emudata() {
 function main() {
   export _script="$0"
   defaults
-  mk_dirs
-  setup_flatpak 'Nestopia-UE' 'ca._0ldsk00l.Nestopia'
+  #safe_mkdir "$emudata"
+  # Nestopia-UE currently has no way to cleanly quit the emulator from fullscreen
+  # OR to easily set up keybindings.  If someone can figiure out a simple way around
+  # this I'm open to re-instituting it, but at this time the only gamepad-centric way
+  # to use this emulator is though libretro
+  #setup_flatpak 'Nestopia-UE' 'ca._0ldsk00l.Nestopia'
+
+  # Despite all efforts, RetroArch still is one of the only ways to interact wtih an
+  # some emulators in a fully gamepad-centric way.  
+  setup_flatpak 'RetroArch' 'org.libretro.RetroArch'
+  setup_flatpak 'DuckStation' 'org.duckstation.DuckStation'
 }
 
 main "$@"
